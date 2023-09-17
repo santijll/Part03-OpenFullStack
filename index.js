202415ee3a76bde382dea92c92bdd80641634ae7
santijll <130672495+santijll@users.noreply.github.com>
2023-09-17 19:43:05 +0200

    adding dist folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 let persons = [
   {
@@ -27,6 +27,7 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(express.static("dist"));
 
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
